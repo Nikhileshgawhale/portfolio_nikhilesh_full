8f91ebac8a951962447bf46bb96306ff941af57d
Nikhilesh Gawhale <nikhileshgawhale711@gmail.com>
2025-06-14 11:10:05 +0100

    Change Vite config to .js extension

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio_nikhilesh_full/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
 }) 
